a5d5aa92e3262c68b6a6c2431ad8d1f4b6454154
n-kats <n-kats19890214@hotmail.co.jp>
2015-06-17 04:34:25 +0900

    edit editorconfig

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ln -s ~/git/dotfiles/.vimrc ~/.vimrc
-ln -s ~/git/dotfiles/.gvimrc ~/.gvimrc
-ln -s ~/git/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/git/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/git/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/git/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/git/dotfiles/.editorconfig ~/.editorconfig
 
--- install_force.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ln -sf ~/git/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/git/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/git/dotfiles/.zshrc ~/.zshrc
-
