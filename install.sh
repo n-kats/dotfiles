06f4ff5eaf22999214ae5609f16b04222b834b9b
n-kats <n-kats19890214@hotmail.co.jp>
2015-12-25 10:15:25 +0900

    install-script for nvim/init.vim

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ ln -sf ~/git/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/git/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/git/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/git/dotfiles/.editorconfig ~/.editorconfig
-
+ln -sf ~/git/dotfiles/.vimrc ~/.config/nvim/init.vim
