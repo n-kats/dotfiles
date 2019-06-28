24bccbe3840f9429712e372759a906d0902b5ba7
n-kats <n-kats19890214@hotmail.co.jp>
2019-06-29 05:29:39 +0900

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 ANYENV=~/.anyenv/bin/anyenv
 PYENV=~/.anyenv/envs/pyenv/bin/pyenv
 $ANYENV update
+exec "$($ANYENV init -)"
 $PYENV shell 3.7.3
 pip install -U pip
 pip install -U setuptools pipenv neovim
