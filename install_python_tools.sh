fc3cf5e537de5c0ddde38af8f738fadd13d3b511
n-kats <n-kats19890214@hotmail.co.jp>
2021-11-23 01:09:06 +0900

    wip

--- install_python_tools.sh
+++ install_python_tools.sh
@@ -53,7 +53,7 @@ EOS
 # poetry
 setup poetry \
   3.8.12 \
-  poetry pynvim python-language-server poetry-dynamic-versioning webencodings
+  poetry pynvim python-language-server poetry-dynamic-versioning webencodings pysen-ls
 
 add_command poetry poetry zsh
 add_command poetry-dynamic-versioning poetry zsh
