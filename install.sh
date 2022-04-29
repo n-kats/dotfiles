57fbc2a8ece2dbe605bb14be537441062681f8d4
n-kats <n-kats19890214@hotmail.co.jp>
2022-04-29 16:01:33 +0900

    tigrc追加

--- install.sh
+++ install.sh
@@ -1,50 +1,50 @@
 #! /bin/bash
 set -eu
-cd `dirname $0`
+cd "$(dirname "$0")"
 
 # zsh
-ln -sf `pwd`/.zshrc ~/.zshrc
+ln -sf "$PWD/.zshrc" "$HOME/.zshrc"
 
 # editorconfig
-ln -sf `pwd`/.editorconfig ~/.editorconfig
+ln -sf "$PWD/.editorconfig" "$HOME/.editorconfig"
 
 # nvim
-if [ ! -e ~/.config/nvim ]; then
-  mkdir -p ~/.config/nvim
-fi
-
-ln -sf `pwd`/init.vim ~/.config/nvim/init.vim
-ln -sf `pwd`/dein.toml ~/.config/nvim/dein.toml
+mkdir -p "$HOME/.config/nvim"
+ln -sf "$PWD/init.vim" "$HOME/.config/nvim/init.vim"
+ln -sf "$PWD/dein.toml" "$HOME/.config/nvim/dein.toml"
 
 # procs
-mkdir -p ~/.config/procs
-ln -sf `pwd`/procs/config.toml ~/.config/procs/config.toml
+mkdir -p "$HOME/.config/procs"
+ln -sf "$PWD/procs/config.toml" "$HOME/.config/procs/config.toml"
+
+# Tig
+ln -sf "$PWD/.tigrc" "$HOME/.tigrc"
 
 # anyenv
-ANYENV_ROOT=~/.anyenv
-ANYENV_PLUGINS=~/.anyenv/plugins
-ANYENV=~/.anyenv/bin/anyenv
-PYENV=~/.anyenv/envs/pyenv/bin/pyenv
+ANYENV_ROOT="$HOME/.anyenv"
+ANYENV_PLUGINS="$HOME/.anyenv/plugins"
+ANYENV="$HOME/.anyenv/bin/anyenv"
+PYENV="$HOME/.anyenv/envs/pyenv/bin/pyenv"
 
-if [ ! -e $ANYENV_ROOT ]; then
-  git clone https://github.com/anyenv/anyenv $ANYENV_ROOT
-  yes | $ANYENV install --init
+if [[ ! -e "$ANYENV_ROOT" ]]; then
+  git clone https://github.com/anyenv/anyenv "$ANYENV_ROOT"
+  yes | "$ANYENV" install --init
   eval "$($ANYENV init -)"
-  $ANYENV install pyenv
+  "$ANYENV" install pyenv
   eval "$($ANYENV init -)"
 fi
 
-if [ ! -e $ANYENV_PLUGINS ]; then
-  mkdir -p $ANYENV_PLUGINS
+if [[ ! -e "$ANYENV_PLUGINS" ]]; then
+  mkdir -p "$ANYENV_PLUGINS"
 fi
 
-if [ ! -e $ANYENV_PLUGINS/anyenv-update ]; then
-  mkdir -p $ANYENV_PLUGINS
-  git clone https://github.com/znz/anyenv-update.git $ANYENV_PLUGINS/anyenv-update
+if [ ! -e "$ANYENV_PLUGINS/anyenv-update" ]; then
+  mkdir -p "$ANYENV_PLUGINS"
+  git clone https://github.com/znz/anyenv-update.git "$ANYENV_PLUGINS/anyenv-update"
 fi
 
-if [ ! $($PYENV versions | grep 3.8.5 > /dev/null && echo 1) ]; then
-  $PYENV install 3.8.5
+if [[ ! "$($PYENV versions | grep 3.8.5 > /dev/null && echo 1)" ]]; then
+  "$PYENV" install 3.8.5
 fi
 
 ./update.sh
